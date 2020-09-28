6f8f9591004367780abb082d64d0b4f84a3f7d6b
safiullaansariti <safiulla2016ansari@gmail.com>
2020-09-28 13:40:46 +0530

    Change from git

--- MVCDemoApp/Controllers/EmployeeController.cs
+++ MVCDemoApp/Controllers/EmployeeController.cs
@@ -11,7 +11,7 @@ namespace MVCDemoApp.Controllers
         // GET: Employee
         public ActionResult Index()
         {
-            return View();
+            return View("Index");
         }
     }
 }
