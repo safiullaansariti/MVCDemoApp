04099e3f9cfd8a19b7312288a4e0dd32c69c43fa
Safiulla Ansari <safiulla.a@9gridtech.com>
2020-09-28 12:33:13 +0530

    Add project files.

---
+++ MVCDemoApp/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCDemoApp.Models
+{
+    public class Employee
+    {
+        public void Method1()
+        {
+            Console.WriteLine("I Done");
+        }
+    }
+}
